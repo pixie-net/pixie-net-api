80aefe8df6833fcc35f6bed457436499d60489b2
S.V. Paulauskas <stanpaulauskas@gmail.com>
2020-02-22 10:33:32 -0500

    Moving pixie-net directory to API
    Should remove confusion as to what the directory contains.

--- src/api/ProgramFippi.cpp
+++ src/api/ProgramFippi.cpp
@@ -1,65 +1,24 @@
-/*----------------------------------------------------------------------
- * Copyright (c) 2017 XIA LLC
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, 
- * with or without modification, are permitted provided 
- * that the following conditions are met:
- *
- *   * Redistributions of source code must retain the above 
- *     copyright notice, this list of conditions and the 
- *     following disclaimer.
- *   * Redistributions in binary form must reproduce the 
- *     above copyright notice, this list of conditions and the 
- *     following disclaimer in the documentation and/or other 
- *     materials provided with the distribution.
- *   * Neither the name of XIA LLC
- *     nor the names of its contributors may be used to endorse 
- *     or promote products derived from this software without 
- *     specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND 
- * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, 
- * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF 
- * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
- * IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE 
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON 
- * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR 
- * TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF 
- * THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF 
- * SUCH DAMAGE.
- *----------------------------------------------------------------------*/
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <math.h>
-#include <time.h>
-#include <signal.h>
-#include <assert.h>
-#include <errno.h>
-#include <string.h>
+/// @file ProgramFippi.cpp
+/// @brief Uses the provided configuration file to program the FPGA.
+/// @author S. V. Paulauskas
+/// @date February 22, 2020
+#include "ProgramFippi.hpp"
+
+#include <cmath>
+#include <cstdio>
+
 #include <sys/mman.h>
 #include <sys/file.h>
+#include <unistd.h>
 
+#include "PixieNetCommon.hpp"
+#include "PixieNetConfig.hpp"
 
-// need to compile with -lm option
-
-#include "PixieNetDefs.h"
-#include "PixieNetCommon.h"
-#include "PixieNetConfig.h"
-
-
-int main(void) {
+int ProgramFippi::program_fippi(const char *settings) {
     int size = 4096;
     int k, addr;
     
-    PixieNetFippiConfig fippiconfig = InitializeFippi("settings.ini");
+    PixieNetFippiConfig fippiconfig = InitializeFippi(settings);
     
     unsigned int mval, dac;
     unsigned int CW, FR, SL[NCHANNELS], SG[NCHANNELS], FL[NCHANNELS], FG[NCHANNELS], TH[NCHANNELS];
@@ -78,7 +37,6 @@ int main(void) {
     // first, set CSR run control options
     mapped[ACSRIN] = 0x0000; // all off
     mapped[AOUTBLOCK] = OB_IOREG;      // read from IO block
-    
     
     // take struct values, convert to FPGA units, write to register, one by one
     // error/dependency check: returns -xxyy,
@@ -703,13 +661,3 @@ int main(void) {
     close(fd);
     return 0;
 }
-
-
-
-
-
-
-
-
-
-
